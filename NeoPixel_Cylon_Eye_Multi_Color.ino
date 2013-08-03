333972c9e952acf55a22bc646027303e1eefb6ab
EternalCore <im_your_fixit_man@hotmail.com>
2013-08-02 21:34:28 -0600

    Added Video and Script used in the video.

---
+++ NeoPixel_Cylon_Eye_Multi_Color.ino
@@ -0,0 +1,102 @@
+//NeoPixel LED Digital Strip Cylon Eye v1.03 Created by EternalCore
+#include <Adafruit_NeoPixel.h>
+
+//Settings:
+#define PIN 6 //The Pin out your Neopixel DIN strip/stick is connected to (Default is 6)
+#define TPIXEL 60 //The total amount of pixel's/led's in your connected strip/stick (Default is 60)
+int wait_T=40; //This is the delay between moving back and forth and per pixel
+int PixelCount=30; //Set this to the AMOUNT of Led's/Pixels you have or want to use on your strip And It can be used to tell where to Stop then return the eye at in the strip
+int Pixel_Start_End=0; //Set this to where you want it to Start/End at
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_RGB     Pixels are wired for RGB bitstream
+//   NEO_GRB     Pixels are wired for GRB bitstream
+//   NEO_KHZ400  400 KHz bitstream (e.g. FLORA pixels)
+//   NEO_KHZ800  800 KHz bitstream (e.g. High Density LED strip)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(TPIXEL, PIN, NEO_GRB + NEO_KHZ800); //Standered Strip function
+
+void setup() {
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
+  //Serial.begin(9600); //Used For pixel Count Debugging
+}
+void loop() {
+	//Loop 2x for Red
+  for(int l=0; l<2; l++) {
+  //Example: CylonEyeUp(Center_Dot_Color, Second_Dot_color, Third_Dot_color, wait_T, PixelCount, Pixel_Start_End);
+  CylonEyeUp(strip.Color(175,0,0), strip.Color(25,0,0), strip.Color(10,0,0), wait_T, PixelCount, Pixel_Start_End);
+  delay(wait_T);
+  //Example: CylonEyeDown(Center_Dot_Color, Second_Dot_color, Third_Dot_color, wait_T, PixelCount, Pixel_Start_End);
+  CylonEyeDown(strip.Color(175,0,0), strip.Color(25,0,0), strip.Color(10,0,0), wait_T, PixelCount, Pixel_Start_End);
+  delay(wait_T);
+  }
+	//Loop 2x for Green
+  for(int l=0; l<2; l++) {
+  //Example: CylonEyeUp(Center_Dot_Color, Second_Dot_color, Third_Dot_color, wait_T, PixelCount, Pixel_Start_End);
+  CylonEyeUp(strip.Color(0,175,0), strip.Color(0,25,0), strip.Color(0,10,0), wait_T, PixelCount, Pixel_Start_End);
+  delay(wait_T);
+  //Example: CylonEyeDown(Center_Dot_Color, Second_Dot_color, Third_Dot_color, wait_T, PixelCount, Pixel_Start_End);
+  CylonEyeDown(strip.Color(0,175,0), strip.Color(0,25,0), strip.Color(0,10,0), wait_T, PixelCount, Pixel_Start_End);
+  delay(wait_T);
+  }
+  	//Loop 2x for Blue
+    for(int l=0; l<2; l++) {
+  //Example: CylonEyeUp(Center_Dot_Color, Second_Dot_color, Third_Dot_color, wait_T, PixelCount, Pixel_Start_End);
+  CylonEyeUp(strip.Color(0,0,175), strip.Color(0,0,25), strip.Color(0,0,10), wait_T, PixelCount, Pixel_Start_End);
+  delay(wait_T);
+  //Example: CylonEyeDown(Center_Dot_Color, Second_Dot_color, Third_Dot_color, wait_T, PixelCount, Pixel_Start_End);
+  CylonEyeDown(strip.Color(0,0,175), strip.Color(0,0,25), strip.Color(0,0,10), wait_T, PixelCount, Pixel_Start_End);
+  delay(wait_T);
+  }
+  	//Loop 2x for Red, Green and Blue
+    for(int l=0; l<2; l++) {
+  //Example: CylonEyeUp(Center_Dot_Color, Second_Dot_color, Third_Dot_color, wait_T, PixelCount, Pixel_Start_End);
+  CylonEyeUp(strip.Color(175,0,0), strip.Color(0,25,0), strip.Color(0,0,10), wait_T, PixelCount, Pixel_Start_End);
+  delay(wait_T);
+  //Example: CylonEyeDown(Center_Dot_Color, Second_Dot_color, Third_Dot_color, wait_T, PixelCount, Pixel_Start_End);
+  CylonEyeDown(strip.Color(175,0,0), strip.Color(0,25,0), strip.Color(0,0,10), wait_T, PixelCount, Pixel_Start_End);
+  delay(wait_T);
+  }
+  	//Loop 2x for White
+      for(int l=0; l<2; l++) {
+  //Example: CylonEyeUp(Center_Dot_Color, Second_Dot_color, Third_Dot_color, wait_T, PixelCount, Pixel_Start_End);
+  CylonEyeUp(strip.Color(175,175,175), strip.Color(25,25,25), strip.Color(10,10,10), wait_T, PixelCount, Pixel_Start_End);
+  delay(wait_T);
+  //Example: CylonEyeDown(Center_Dot_Color, Second_Dot_color, Third_Dot_color, wait_T, PixelCount, Pixel_Start_End);
+  CylonEyeDown(strip.Color(175,175,175), strip.Color(25,25,25), strip.Color(10,10,10), wait_T, PixelCount, Pixel_Start_End);
+  delay(wait_T);
+  }
+}
+
+void CylonEyeUp(uint32_t Co, uint32_t Ct, uint32_t Ctt, uint8_t Delay, int TotalPixels, int pStart) {
+  for(int i=pStart; i<TotalPixels; i++) {
+    strip.setPixelColor(i+2, Ctt);  //Third Dot Color
+    strip.setPixelColor(i+1, Ct);   //Second Dot Color
+    strip.setPixelColor(i, Co);     //Center Dot Color
+    strip.setPixelColor(i-1, Ct);   //Second Dot Color
+    strip.setPixelColor(i-2, Ctt);  //Third Dot Color
+
+    strip.setPixelColor(i-3, strip.Color(0,0,0)); //Clears the dots after the 3rd color
+    strip.show();
+    //Serial.println(i); //Used For pixel Count Debugging
+    delay(Delay);
+
+  }
+}
+void CylonEyeDown(uint32_t Co, uint32_t Ct, uint32_t Ctt, uint8_t Delay, int TotalPixels, int pEnd) {
+  for(int i=TotalPixels-1; i>pEnd; i--) {
+    strip.setPixelColor(i-2, Ctt);  //Third Dot Color
+    strip.setPixelColor(i-1, Ct);   //Second Dot Color
+    strip.setPixelColor(i, Co);    //Center Dot Color
+    strip.setPixelColor(i+1, Ct);  //Second Dot Color
+    strip.setPixelColor(i+2, Ctt); //Third Dot Color
+
+    strip.setPixelColor(i+3, strip.Color(0,0,0)); //Clears the dots after the 3rd color
+    strip.show();
+    //Serial.println(i); //Used For pixel Count Debugging
+    delay(Delay);
+  }
+}
+
+
